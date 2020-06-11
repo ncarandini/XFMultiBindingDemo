e9aee816f34d0682b642855d03ebe5a904bc263e
Nicolò Carandini <n.carandini@outlook.com>
2020-06-12 00:13:41 +0200

    Add solution

---
+++ MultiBindingDemo/MultiBindingDemo/Converters/AllMustBeTrueMultiConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MultiBindingDemo.Converters
+{
+    public class AllMustBeTrueMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = true;
+
+            foreach (object value in values)
+            {
+                if (value is bool)
+                {
+                    result = result && (bool)value;
+                }
+                else
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MultiBindingDemo/MultiBindingDemo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiBindingDemo.ViewModels
+{
+    public class MainPageViewModel : ObservableObject
+    {
+        bool conditionA = false;
+        public bool ConditionA
+        {
+            get => conditionA;
+            set => SetProperty(ref conditionA, value, onChanged: () =>
+            {
+                OnPropertyChanged(nameof(ConditionAandB));
+            });
+        }
+
+        bool conditionB = true;
+        public bool ConditionB
+        {
+            get => conditionB;
+            set => SetProperty(ref conditionB, value, onChanged: () =>
+            {
+                OnPropertyChanged(nameof(ConditionAandB));
+            });
+        }
+
+        public bool ConditionAandB => ConditionA && ConditionB;
+    }
+}
